7c62f59ee8cbf7147b1425515c648508bbfe155d
wendiyou <1129307781@qq.com>
2021-01-16 13:15:22 +0800

    nacos spring cloud provider 调通

---
+++ nacos-server-spring-cloud/src/test/java/com/study/nacosserverspringcloud/NacosServerSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.nacosserverspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosServerSpringCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
